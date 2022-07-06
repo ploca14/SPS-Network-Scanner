fcb7193818c60101858b32bd28502a892c408da4
Jakub Švorc <svorcjak@fel.cvut.cz>
2022-07-06 13:48:05 +0200

    Untested dictionary to pair IP and time.

--- scanner.py
+++ scanner.py
@@ -1,19 +1,22 @@
-#! /usr/bin/env python
+#! /usr/bin/sudo python
 from scapy.all import *
 import logging
 
 logging.basicConfig(level=logging.DEBUG)
 
 cache = []
+inMemoryAddrDB = {}
 
 def arp_monitor_callback(pkt):
     cache.append(pkt)
+    inMemoryAddrDB[ARP.hwsrc] = datetime.now()
     logging.info(pkt.sprintf(" Sniffed arp packet from: %ARP.hwsrc% %ARP.psrc%"))
 
 def onShutdown():
-    wrpcap("packetlog" + datetime.now().strftime('%Y-%m-%d') + ".pcap", cache)
+    if len(cache) > 0:
+        wrpcap("packetlog" + datetime.now().strftime('%Y-%m-%d') + ".pcap", cache)
 
 atexit.register(onShutdown)
 
 sniff(prn=arp_monitor_callback, filter="arp", store=0)
-#wrpcap na ulozeni historie na disk (pokud bude program resetovany)
+
