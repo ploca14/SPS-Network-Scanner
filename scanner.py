aa54c6940274fbd3f099784d40e89e69718d8aec
Jakub Švorc <svorcjak@fel.cvut.cz>
2022-07-06 13:34:06 +0200

    Saving packets to pcap - wireshark testing.

--- scanner.py
+++ scanner.py
@@ -5,13 +5,15 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 
 cache = []
-def updateCache(pkt):
-    cache.append(pkt)
 
-def arp_monitor_callback(pkt): #who-has or is-at
-    updateCache(pkt)
+def arp_monitor_callback(pkt):
+    cache.append(pkt)
     logging.info(pkt.sprintf(" Sniffed arp packet from: %ARP.hwsrc% %ARP.psrc%"))
 
+def onShutdown():
+    wrpcap("packetlog" + datetime.now().strftime('%Y-%m-%d') + ".pcap", cache)
+
+atexit.register(onShutdown)
 
 sniff(prn=arp_monitor_callback, filter="arp", store=0)
 #wrpcap na ulozeni historie na disk (pokud bude program resetovany)
