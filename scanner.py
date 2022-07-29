46a2e5fc9ffea1df7e93ab100f1eb9fb8d233f06
Jakub Svorc <svorcjak@fel.cvut.cz>
2022-07-29 19:52:04 +0200

    logging

--- scanner.py
+++ scanner.py
@@ -9,14 +9,17 @@ inMemoryAddrDB = {}
 
 def arp_monitor_callback(pkt):
     cache.append(pkt)
-    inMemoryAddrDB[ARP.hwsrc] = datetime.now()
+    inMemoryAddrDB[pkt.sprintf("%ARP.psrc%")] = datetime.now()
     logging.info(pkt.sprintf(" Sniffed arp packet from: %ARP.hwsrc% %ARP.psrc%"))
+
 
 def onShutdown():
     if len(cache) > 0:
+        logging.info("Saving packet log at: " + datetime.now().strftime('%H:%M %Y-%m-%d'))
+        print(inMemoryAddrDB)
         wrpcap("packetlog" + datetime.now().strftime('%Y-%m-%d') + ".pcap", cache)
+
 
 atexit.register(onShutdown)
 
 sniff(prn=arp_monitor_callback, filter="arp", store=0)
-
